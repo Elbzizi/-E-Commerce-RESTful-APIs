86477ab7c1cdf7ab8b2f97ca747ed4c971063d7c
omar <omar@tzst.com>
2024-08-20 22:34:26 +0100

    create config file

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 express = require('express')
 app = express()
-
+dotenv = require('dotenv')
+dotenv.config({ path: 'config.env' })
 app.get('/', (req, res) => {
   res.send('<h1>hello Api</h1>')
 })
-app.listen('8000', () => {
-  console.log(`server  is raning to port : ${8000}`)
+port = process.env.PORT
+app.listen(port, () => {
+  console.log(`server  is raning to port : ${port}`)
 })
